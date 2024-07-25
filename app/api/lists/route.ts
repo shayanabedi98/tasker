ed2692460a08b6e1271409db9b849c94830af57d
shayanabedi98 <shayanabedi@hotmail.com>
2024-07-24 21:32:05 -0400

    updated routes for deployment

--- app/api/lists/route.ts
+++ app/api/lists/route.ts
@@ -1,7 +1,6 @@
 import prisma from "@/lib/prismadb";
 import { getServerSession } from "next-auth";
-import { authOptions } from "../auth/[...nextauth]/route";
-import { User } from "@/types/types";
+import { authOptions } from "../auth/[...nextauth]/options";
 import { NextResponse } from "next/server";
 
 export async function GET(req: Request) {
